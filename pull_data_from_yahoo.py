00e5042fb0967c6b1ef70f7a0acd8a6bc30ec417
abshughes <anna.ba.hughes@gmail.com>
2016-09-14 23:59:03 -0700

    got porsgres setup

--- pull_data_from_yahoo.py
+++ pull_data_from_yahoo.py
@@ -1,6 +1,22 @@
 from yahoo_finance import Share
+import json
+import pandas as pd
+from pandas.io.json import json_normalize
+
+from urllib2 import Request, urlopen
+import json
+
+
 yahoo = Share('YHOO')
-print yahoo.get_open()
-print yahoo.get_price()
-print yahoo.get_trade_datetime()
-print yahoo.get_historical('2014-04-25', '2014-04-29')
+#print yahoo.get_open()
+#print yahoo.get_price()
+#print yahoo.get_trade_datetime()
+#print yahoo.get_historical('2014-04-25', '2014-04-29')
+
+
+##df=pd.read_json(yahoo.splitlines())
+#print "this is pandas now"
+
+list_of_rates=yahoo.get_historical('2014-04-25', '2016-04-28')
+print list_of_rates
+print pd.DataFrame(list_of_rates)
